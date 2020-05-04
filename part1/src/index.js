7853f47810ff7f3200c91a46f241ab951eec730d
shlevanto <simo.levanto@helsinki.fi>
2020-05-04 18:37:50 +0300

    part 0 tehtävät

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  console.log("Hello from komponentti")
+  return (
+  <div>
+    <p>Hello World, it is {now.toString()}</p>
+    <p>
+      {a} plus {b} on {a + b}
+    </p>
+  </div>
+  )
+  }
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +23,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
