1e22c97f6264479e243c7d9198f91f4ae5df1f92
shlevanto <simo.levanto@helsinki.fi>
2020-05-06 15:37:33 +0300

    tehtävä 1.5 tehty

--- part1/kurssitiedot/src/index.js
+++ part1/kurssitiedot/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// seuraava tehtävä 1.4
 
 const Header = (props) => {
   return (
@@ -34,29 +33,31 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  
-  const parts = [
-    {  name: 'Fundamentals of React',
-      exercises:  10
-    },
+  const course = {
+    name: 'Half Stack application development'
+    , 
+    parts: [
+      {  name: 'Fundamentals of React',
+        exercises:  10
+      },
 
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
 
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-]
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+  ]
+  }
 
   return (
     <div>
-      <Header course = {course} />    
-      <Content parts = {parts}/>
-      <Total parts = {parts} />
+      <Header course = {course.name} />    
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts} />
     </div>
   )
 }
