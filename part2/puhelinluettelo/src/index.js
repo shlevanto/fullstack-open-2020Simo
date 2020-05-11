31cdaf569c7f96bd704278471ddfef4249eb875f
shlevanto <simo.levanto@helsinki.fi>
2020-05-11 10:40:08 +0300

    2.7. tehty kömpelöllä forEach toteutuksella

--- part2/puhelinluettelo/src/index.js
+++ part2/puhelinluettelo/src/index.js
@@ -6,7 +6,7 @@ const App = () => {
   const [ persons, setPersons ] = useState([
     {name: 'Arto Hellas'}
   ]) 
-  const [ newName, setNewName ] = useState('..')
+  const [ newName, setNewName ] = useState('')
   
   const Person = (props) => {
     return (
@@ -14,19 +14,45 @@ const App = () => {
     )
   }
 
+  
   const addName = (event) => {
+    
+   
+
     event.preventDefault()
+    
     const nameObject = {
       name: newName
     }
 
+    let inBook = false
+      
+    const checkName = (person) => {
+      return person.name == nameObject.name
+    }
+
+
+    persons.forEach(person => {
+      
+      if (checkName(person) == true) {
+        inBook = true
+        return inBook
+      }
+    })
+
+    if (inBook) {   
+      window.alert(`${newName} is already added in phonebook`)
+      return
+    }
     setPersons(persons.concat(nameObject))
   }
 
   const handleNewName = (event) => {
+    
     setNewName(event.target.value)
 
   }
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -44,7 +70,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
-        <Person name = {person.name}/>)}
+        <Person key = {person.name} name = {person.name}/>)}
       </div>
     </div>
   )
