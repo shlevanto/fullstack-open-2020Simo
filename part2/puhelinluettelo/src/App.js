5deb0f4c65a4a18af32757b3fe822e906a5d7803
shlevanto <simo.levanto@helsinki.fi>
2020-05-12 10:55:29 +0300

    2.11 tehty

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilterForm from './components/FilterForm'
 import Persons from './components/Persons'
 
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ Searched, setSearched ] = useState('')
   const [ showAll, setShowAll ] = useState(true)
   
+  useEffect(() =>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+
+  },[])
+
   const addPerson = (event) => { 
     event.preventDefault()
     
