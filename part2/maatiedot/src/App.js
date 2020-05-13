bdc5e01ede32d13b36b1fd3d0a21b31fec7434ac
shlevanto <simo.levanto@helsinki.fi>
2020-05-13 12:01:33 +0300

    2.14 tehty

--- part2/maatiedot/src/App.js
+++ part2/maatiedot/src/App.js
@@ -22,6 +22,8 @@ const FullCountry = ({ country }) => {
                     <li key={language.iso639_1}>{language.name}</li>)}
             </ul>
             <img src={country.flag} alt='flag' width = {300} decoding = "sync" />
+            <h2>Weather in {country.capital}</h2>
+            <Weather capital = {country.capital}/>
         </div>
 
     )
@@ -73,12 +75,61 @@ const Countries = (props) => {
     )
 
 }
+const Weather = (props) => {
+    const location = props.capital 
+    console.log(location);
+    
+    
+    const [weather, setWeather] = useState(null)
+    
+ 
+    useEffect(() => {
+        axios
+        .get('http://api.weatherstack.com/current',{
+            params: {
+                access_key: process.env.REACT_APP_WEATHER,
+                query: location,
+                units: 'm'
+            }
+        })
+        .then(response  => {
+            setWeather(response.data.current)
+        })
+    },[])
+    
+    
+    if (weather === null) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+    
+    console.log(weather.observation_time);
+    
+    return (
+        <div>
+            <WeatherInfo weather = {weather}/>
+        </div>
+    )
+    
 
+}
+
+const WeatherInfo = ({weather}) => {
+    
+    return (
+        <div>
+        <p><b>temperature:</b> {weather.temperature} Celsius</p>
+        <p><b>wind: </b> {weather.wind_speed} kmh direction {weather.wind_dir}</p>
+        <img src = {weather.weather_icons} alt = 'weather icon' width = {100}/> 
+        </div>
+    )
+} 
 
 
 const App = () => {
     const [countries, setCountries] = useState(null)
-    const [finder, setFinder] = useState('swi')
+    const [finder, setFinder] = useState('')
 
     const handleFinder = (event) => {
         console.log(event.target.value);
