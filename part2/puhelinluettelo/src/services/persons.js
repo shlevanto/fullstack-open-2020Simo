6bfaf7e8edbb356d5066a959540cfcfbfb14cb28
shlevanto <simo.levanto@helsinki.fi>
2020-05-15 10:32:55 +0300

    puhelinluettelon service unohtui gitistä, lisätty

---
+++ part2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const baseURL = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(baseURL)
+
+    return (
+        request.then(response => response.data)
+    )
+
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseURL, newObject)
+
+    return (
+        request.then(response => response.data)
+    )
+}
+
+const remove = (id) => {
+    
+
+    return (
+        axios.delete(`${baseURL}/${id}`)
+
+    )
+}
+
+const update = (id, newObject) => {
+        
+    return axios.put(`${baseURL}/${id}`, newObject)
+}
+
+export default {getAll, create, remove, update}
