344bb051dd04442c31b445d909841b7c0e1970ee
shlevanto <simo.levanto@helsinki.fi>
2020-08-04 08:54:06 +0300

    4.15. tehty ja siivottu

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,18 +4,14 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (req, res) => {
   const body = req.body
-
-  console.log(body.password)
   
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
-
-  console.log(passwordHash)
   
   const user = new User({
     username: body.username,
     name: body.name,
-    passWordHash: passwordHash
+    passwordHash
   })
 
   const savedUser = await user.save()
