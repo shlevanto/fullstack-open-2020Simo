29bcead5a1a05838065ab76258c6c59b0b303001
shlevanto <simo.levanto@helsinki.fi>
2020-08-11 16:08:51 +0300

    5.1. tehty

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,13 +17,75 @@ const App = () => {
     )  
   }, [])
 
+  const loginForm = () => (
+    <form onSubmit = {handleLogin}>
+      <h2>Login</h2>
+      <div>
+        username
+          <input
+          type = "text"
+          value = {username} 
+          name = "Username"
+          onChange = {({ target }) => setUsername(target.value)}
+          />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, '+', password)
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+    
+    setUser(user)
+    setUsername('')
+    setPassword('')
+
+    console.log(user)
+    
+    } catch (exception) {
+      setErrorMessage('incorrect username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 2000)
+      
+    }
+    
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+  <div>
+    <h1>Bloglist</h1>
+    
+    <Notification message = {errorMessage} />
+    
+    {user === null ?
+     loginForm() :
+
+     <div>
+       <p>{user.name} logged in</p>
+       <h2>blogs</h2>
+       {blogs.map(blog =>
+       <Blog key={blog.id} blog={blog} />
+       )}
+     </div>
+    }
+
+  </div>
   )
 }
 
