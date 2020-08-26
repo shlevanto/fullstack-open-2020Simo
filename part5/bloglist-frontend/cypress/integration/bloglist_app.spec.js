f36a25a19b3941f175f98dfce73121ebfc817824
shlevanto <simo.levanto@helsinki.fi>
2020-08-26 16:40:08 +0300

    5.17 tehty

---
+++ part5/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -0,0 +1,35 @@
+describe('Bloglist app', function() {
+  // lue tämä osio materiaalista ensin
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      username: 'tepi',
+      password: 'hupi',
+      name: 'TestiTeppo'
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/users', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('opens login page', function() {
+    cy.contains('Bloglist')
+    cy.contains('Login')
+  })
+  
+  it('can not log in with incorrect credentials', function() {
+    cy.get('#username').type('Bob')
+    cy.get('#password').type('hack')
+    cy.get('#login-button').click()
+    cy.contains('incorrect username or password')
+  })
+
+  it('can log in with existing user', function() {
+    cy.get('#username').type('tepi')
+    cy.get('#password').type('hupi')
+    cy.get('#login-button').click()
+  })
+
+  
+})
