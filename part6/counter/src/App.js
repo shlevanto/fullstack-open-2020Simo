0e1074c33904615fc683203a5b8b5f5972b8f38f
shlevanto <simo.levanto@helsinki.fi>
2020-08-29 11:06:18 +0300

    counterilla harjoittelua, osa 6

--- part6/counter/src/App.js
+++ part6/counter/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO': 
+      return 0
+    default:
+      return state
+  }
 }
 
-export default App;
+const store  = createStore(counterReducer)
+console.log(store.getState())
+store.dispatch({ type: 'INCREMENT' })
+console.log(store.getState())
+
+const App = () => {
+  return(
+    <div>
+      <div>
+        {store.getState()}
+      </div>
+      <button id='plus'
+        onClick={e => store.dispatch({ type: 'INCREMENT' })}
+      >
+        plus
+      </button>
+      <button id='minus'
+        onClick={e => store.dispatch({ type: 'DECREMENT' })} 
+      >
+        minus
+      </button>
+      <button id='zero'
+        onClick={e => store.dispatch({ type: 'ZERO'})}
+        >
+          zero
+        </button>
+    </div>
+  )
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root')) 
+}
+
+renderApp()
+store.subscribe(renderApp)
+
+export default App
