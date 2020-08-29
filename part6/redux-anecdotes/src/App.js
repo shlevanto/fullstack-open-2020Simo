fc6cf151802c72606229b31df0b554e83ac55db7
shlevanto <simo.levanto@helsinki.fi>
2020-08-29 22:09:31 +0300

    6.3. tehty

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteFor(id))
+  }
+
+  const voteFor = (id) => {
+    return {
+      type: 'VOTE',
+      data: { id }
+    }
+  }
+
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes} votes
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
