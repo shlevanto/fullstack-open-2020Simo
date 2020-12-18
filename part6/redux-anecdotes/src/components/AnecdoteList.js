684b9cf82dc89e00664daec6e5cdf0ad7928d2e1
shlevanto <simo.levanto@helsinki.fi>
2020-12-18 13:37:55 +0200

    poistettu turhia importeja

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { anecdoteReducer, voteFor } from '../reducers/anecdoteReducer'
+import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
 
