a4ab80c3d5d9c92cc7d75b22bc9244d69dc1a3af
shlevanto <simo.levanto@helsinki.fi>
2020-12-21 18:10:52 +0200

    6.13 tehty ja siirretty alustus App.js hoidettavaksi

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    anecdoteService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  })
 
   return (
     <div>
